d6692c581e521c52a0c165444025e2178f44b8b6
zry <1931860504@qq.com>
2022-03-16 00:27:44 +0800

    add template

---
+++ template/template.c
@@ -0,0 +1,11 @@
+//
+// Created by Matrix on 2022/3/15.
+//
+#include <string.h>
+#include <math.h>
+#include <stdio.h>
+int main()
+{
+
+    return 0;
+}
---
+++ template/template.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Matrix on 2022/3/15.
+//
+using namespace std;
+#include <iostream>
+#include <cstdio>
+int main()
+{
+
+    return 0;
+}
